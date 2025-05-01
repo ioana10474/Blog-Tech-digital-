8e93dc60891f332339987fa2175d520f0e4c0b4b
ioana10474 <ioana10434@gmail.com>
2025-05-01 13:47:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function openALLARTICLES(){
+    window.location.href = "index.html";
+}
+
+function openTRENDING(){
+    window.location.href = "trend.html";
+}
+
+function openREVIEWS(){
+    window.location.href = "review.html";
+}
+
+function openCONTACT(){
+    window.location.href = "contact.html";
+}
